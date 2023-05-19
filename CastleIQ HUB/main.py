28dde4939cf2094250c99e2941357efde7262baa
OlexiiZinik <3802293@gmail.com>
2023-05-19 16:27:59 +0200

    deleted unused file

--- CastleIQ HUB/main.py
+++ CastleIQ HUB/main.py
@@ -1,7 +1,7 @@
 from api import app
 from config import conf
-from database_manager import init_db
 from logger import logger
+from tortoise.contrib.fastapi import register_tortoise
 
 
 @app.on_event('startup')
@@ -11,7 +11,11 @@ def on_startup():
 
 def main():
     if not conf.testing:
-        init_db(app)
+        register_tortoise(
+            app,
+            config=conf.tortoise_conf,
+            add_exception_handlers=True
+        )
 
 
 if __name__ == "main":
